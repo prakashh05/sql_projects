b21613701fa2280bb99ddd24c17cd73afccae106
prakashh05 <prakashhsiva05@gmail.com>
2026-03-15 16:56:03 +0000

    Update choco_sales.sql

--- choco_sales/choco_sales.sql
+++ choco_sales/choco_sales.sql
@@ -1,1 +1,9 @@
+-- The revenue generated each month in each year
+SELECT 
+    EXTRACT(YEAR FROM date::date) AS year,
+    EXTRACT(MONTH FROM date::date) AS month,
+    SUM(REPLACE(REPLACE(amount, '$', ''), ',', '')::numeric) AS total_sales
+FROM choco_sales
+GROUP BY year, month
+ORDER BY year, month;
 
