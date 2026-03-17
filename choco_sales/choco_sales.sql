16c4d6f84b221acdb513ee7ecad5cd5619b7d3f7
prakashh05 <prakashhsiva05@gmail.com>
2026-03-17 17:12:46 +0000

    Delete choco_sales/choco_sales.sql

--- choco_sales/choco_sales.sql
+++
@@ -1,7 +0,0 @@
-SELECT date_part ('month', date::date) AS month, 
-date_part ('year', date::date) AS year, SUM (REPLACE(REPLACE(amount, '$', ''), ',', '') :: numeric) as total_sales
-FROM choco_sales
-WHERE date_part ('year', date::date) BETWEEN 2022 AND 2025
-GROUP BY 1,2
-ORDER BY total_sales DESC
-LIMIT 1
